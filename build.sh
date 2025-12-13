05ca6f89c31776bb5bdae4019a182fc706c0b0ee
flayve23 <flex9digital@gmail.com>
2025-12-12 22:46:11 -0300

    Fix build.sh permissions

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ echo "Installing dependencies..."
 npm install --legacy-peer-deps
 
 echo "Building Vite..."
-./node_modules/.bin/vite build
+npm exec vite build
 
 echo "Building server..."
-./node_modules/.bin/esbuild server/_core/index.ts --platform=node --packages=external --bundle --format=esm --outdir=dist
+npm exec esbuild server/_core/index.ts --platform=node --packages=external --bundle --format=esm --outdir=dist
 
 echo "Build complete!"
