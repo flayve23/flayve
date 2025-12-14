ba4f0067504540c1c816c1e716b6a4735f3a51d3
flayve23 <flex9digital@gmail.com>
2025-12-14 15:03:30 -0300

    Add CORS configuration

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -124,6 +124,8 @@ app.use(cors({
     "http://localhost:5173"
   ],
   credentials: true
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 } ));
   // Segurança: Helmet para headers HTTP
   setupHelmet(app);
